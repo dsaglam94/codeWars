4c4082241d8c8dcfbd82773c3dbb9fdef0b9ec0c
Dogan <dsaglam@Dogan-MacBook-Air.local>
2022-04-02 16:54:45 +0300

    added 7kyus kata solution

--- 8kyus.js
+++ 8kyus.js
@@ -126,3 +126,17 @@ function sumMix(x){
     let newArr = x.map(num => Number(num));
     return newArr.reduce((a,b) => a+b,0);
   }
+
+//   Create a function with two arguments that will return an array of the first (n) multiples of (x).
+
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+// countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+
+function countBy(x, n) {
+    let z = [];
+    for (i = 1; i <= n; i++) {
+        z.push(x * i);
+    }
+    return z;
+}
