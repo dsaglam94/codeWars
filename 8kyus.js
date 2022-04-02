24116e15cefafb6169d4caa261f6102aeb2c405f
Dogan <dsaglam@192.168.1.105>
2022-04-02 10:23:07 +0300

    added new solution

--- 8kyus.js
+++ 8kyus.js
@@ -47,3 +47,7 @@ function sumArray(array) {
       return total;
     }
 }
+
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+const simpleMultiplication = number => (number % 2 === 0) ? (number * 8) : (number * 9)
