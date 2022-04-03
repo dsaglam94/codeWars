ce999b71946ef4f6a0ed8094f7188b5166d8e642
dsaglam94 <d.saglam94@outlook.fr>
2022-04-03 08:38:53 +0300

    added new solution

--- 7kyus.js
+++ 7kyus.js
@@ -203,3 +203,26 @@ function maskify(cc) {
      return hashed + visiblePart
   } 
 }
+
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+
+
+function getSum( a,b )
+{
+  let numArr = [a, b].sort((x,y) => x - y);
+  let allNums = [];
+  for (let i = numArr[0]; i <= numArr[1]; i++){
+    allNums.push(i);
+  }
+
+  return allNums.reduce((x,y) => x + y,0)
+}
+
