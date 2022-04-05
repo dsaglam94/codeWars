504fcf8da7209372f7d0dd5ab6d6c2945ffdf3a2
dsaglam94 <d.saglam94@outlook.fr>
2022-04-05 08:02:24 +0300

    added new 8kyus solution

--- 8kyus.js
+++ 8kyus.js
@@ -140,3 +140,19 @@ function countBy(x, n) {
     }
     return z;
 }
+
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+// Examples
+// remove("Hi!") === "Hi"
+// remove("Hi!!!") === "Hi!!"
+
+function remove (string) {
+  let lastChar = string.length - 1
+  if ( string[lastChar] === '!') {
+    return string.slice(0, -1);
+  } else {
+    return string;
+  }
+}
+
