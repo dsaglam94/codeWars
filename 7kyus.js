862ba81a1cfbc578447a1fc3c3fa4d93ca1b9ea1
dsaglam94 <d.saglam94@outlook.fr>
2022-04-08 07:57:06 +0300

    added another solution 7kyu

--- 7kyus.js
+++ 7kyus.js
@@ -713,3 +713,11 @@ function arithmetic(a, b, operator){
 function sortByLength (array) {
   return array.sort((a,b) => a.length - b.length);
 };
+
+// The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age, oldest age].
+
+function twoOldestAges(ages){
+
+  return ages.sort((a,b) => a - b).slice(-2)
+  
+}
