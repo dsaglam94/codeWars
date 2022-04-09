d008731aec1157b20d1e3c1b59f410303c96635f
dsaglam94 <d.saglam94@outlook.fr>
2022-04-09 08:16:15 +0300

    added new solution

--- 7kyus.js
+++ 7kyus.js
@@ -915,3 +915,22 @@ function checkExam(array1, array2) {
 function smallEnough(a, limit){
   return a.every(a => (a <= limit))
 }
+
+// Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates).
+
+// Notes:
+// Only positive integers will be passed to the function (> 0 ), no negatives or zeros.
+// Input >> Output Examples
+// minValue ({1, 3, 1})  ==> return (13)
+
+function minValue(values){
+  let filteredValues = [...new Set(values)]
+  return parseFloat(filteredValues.sort((a,b) => a - b).join(''));
+}
+
+// diff solution / refactored
+
+function minValue(values){
+  let arr = Array.from(new Set(values))
+  return parseInt(arr.sort().join(''))
+}
