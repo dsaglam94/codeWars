9461c4d2b32692acc4e8a2eadca439ee8692edd2
dsaglam94 <d.saglam94@outlook.fr>
2022-04-10 09:22:42 +0300

    new

--- 7kyus.js
+++ 7kyus.js
@@ -1084,3 +1084,27 @@ function removeDuplicateWords (s) {
   return str;
 
 }
+
+// Write a function called checkCoupon which verifies that a coupon code is valid and not expired.
+
+// A coupon is no more valid on the day AFTER the expiration date. All dates will be passed as strings in this format: "MONTH DATE, YEAR".
+
+// Examples:
+// checkCoupon("123", "123", "July 9, 2015", "July 9, 2015")  ===  true
+// checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
+
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+  
+  return enteredCode === correctCode && new Date(currentDate) <= new Date(expirationDate);
+  
+//   if ( enteredCode === correctCode && new Date(currentDate) <= new Date(expirationDate)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+  
+//   let date1 = new Date(currentDate)
+//   let date2 = new Date(expirationDate)
+  
+//   console.log(date1 <= date2)
+}
