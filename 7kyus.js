d944385115f7dce8c7d10c7a9d9c988ff3663d81
dsaglam94 <d.saglam94@outlook.fr>
2022-04-11 09:27:49 +0300

    added new 7kyu solution

--- 7kyus.js
+++ 7kyus.js
@@ -1157,3 +1157,20 @@ function rowWeights(array){
   }
   return arr;
 }
+
+// Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+
+// Example:
+
+// Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+function flattenAndSort(array) {
+  if (array.length > 0) {
+    let newArr = Array.prototype.concat(...array);
+    
+    return newArr.sort((a,b) => a - b);
+
+  } else {
+    return [];
+  }
+}
