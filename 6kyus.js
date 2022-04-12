ea33df571e32c61c6ca398613c341537b61547fa
dsaglam94 <d.saglam94@outlook.fr>
2022-04-12 07:55:35 +0300

    added new 6-5kyus solution

--- 6kyus.js
+++ 6kyus.js
@@ -159,4 +159,16 @@ function likes(names) {
     }
   }
 
-  
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+// Example
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) 
+
+function createPhoneNumber(numbers){
+    let first = numbers.slice(0,3).join('');
+    let middle = numbers.slice(3,6).join('');
+    let last = numbers.slice(6).join('');
+
+    return `(${first}) ${middle}-${last}`
+
+}
