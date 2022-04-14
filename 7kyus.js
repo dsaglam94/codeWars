303d933890e98421aeaaaeaf6d68bb10d5127a01
dsaglam94 <d.saglam94@outlook.fr>
2022-04-14 08:41:49 +0300

    added another solution

--- 7kyus.js
+++ 7kyus.js
@@ -1465,3 +1465,26 @@ var palindromeChainLength  = function(n) {
   }
   return 0;
 };
+
+// Notes
+// Array/list size is at least 2.
+
+// Array/list numbers could be a mixture of positives, negatives also zeroes .
+
+// Input >> Output Examples
+// adjacentElementsProduct([1, 2, 3]); ==> return 6
+// Explanation:
+// The maximum product obtained from multiplying 2 * 3 = 6, and they're adjacent numbers in the array.
+// adjacentElementsProduct([9, 5, 10, 2, 24, -1, -48]); ==> return 50
+
+function adjacentElementsProduct(array) {
+  
+  let newArr = []
+  
+  for(let i = 0; i < array.length-1; i++){
+    newArr.push(array[i]*array[i+1])
+    console.log(newArr)
+  }  
+  
+  return Math.max(...newArr)
+}
