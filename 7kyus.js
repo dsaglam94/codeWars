db28533beb3ef3557a6911b439de0dad1b495116
dsaglam94 <d.saglam94@outlook.fr>
2022-04-14 09:20:42 +0300

    added another one

--- 7kyus.js
+++ 7kyus.js
@@ -1568,3 +1568,35 @@ function largestPairSum (numbers) {
   let sortedNums = numbers.sort((a,b) => b - a);
   return sortedNums[0] + sortedNums[1]
 }
+
+// Each two non empty parts will be in a pair (or an array for languages without tuples or a structin C - C: see Examples test Cases - )
+// Each part will be in a string
+// Elements of a pair must be in the same order as in the original array.
+// Examples of returns in different languages:
+// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+// [["az", "toto picaro zone kiwi"], ["az toto", "picaro zone kiwi"], ["az toto picaro", "zone kiwi"], ["az toto picaro zone", "kiwi"]] 
+// or
+//  a = {"az", "toto", "picaro", "zone", "kiwi"} -->
+// {{"az", "toto picaro zone kiwi"}, {"az toto", "picaro zone kiwi"}, {"az toto picaro", "zone kiwi"}, {"az toto picaro zone", "kiwi"}}
+// or
+// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+// [("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
+// or 
+// a = [|"az", "toto", "picaro", "zone", "kiwi"|] -->
+// [("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
+// or
+// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+// "(az, toto picaro zone kiwi)(az toto, picaro zone kiwi)(az toto picaro, zone kiwi)(az toto picaro zone, kiwi)"
+
+function partlist(arr) {
+  var newArray;
+  var returnArray=[];
+  for( var i=1; i<arr.length; i++){
+    newArray = [];
+    newArray.push(arr.slice(0,i).join(" "));
+    newArray.push(arr.slice(i).join(" "));
+    returnArray.push(newArray);
+  }
+  
+  return returnArray;
+}
