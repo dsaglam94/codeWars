db22f84964526d6fb5e87169d08542b4f155ce22
dsaglam94 <d.saglam94@outlook.fr>
2022-04-14 07:09:01 +0300

    added new solution

--- 7kyus.js
+++ 7kyus.js
@@ -1389,3 +1389,28 @@ function add(n) {
 function findLongest(array){
   return array.sort((a,b) => (b + '').length - (a + '').length)[0]
 }
+
+// Your task is to return the number of JavaScript developers coming from Europe.
+
+// For example, given the following list:
+
+// var list1 = [
+//   { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+//   { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+//   { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+//   { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+// ];
+// your function should return number 1.
+
+// If, there are no JavaScript developers from Europe then your function should return 0.
+
+function countDevelopers(list) {
+  let count = 0;
+
+  list.forEach(item => {
+  if ( item.continent === 'Europe' && item.language === 'JavaScript' ) {
+    count += 1
+     }
+  });
+  return count;
+}
