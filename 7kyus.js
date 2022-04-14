03abd52002e2eef6d5d6824e82851bc1f00e731d
dsaglam94 <d.saglam94@outlook.fr>
2022-04-14 07:41:54 +0300

    added diff solution

--- 7kyus.js
+++ 7kyus.js
@@ -1455,3 +1455,13 @@ var palindromeChainLength = function(n) {
   }
 
 };
+
+// diff solution with recursive 
+
+var palindromeChainLength  = function(n) {  
+  var x = parseInt( (""+n).split('').reverse().join('') );
+  if(n != x){
+    return 1 + palindromeChainLength (n + x);
+  }
+  return 0;
+};
