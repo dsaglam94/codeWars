fa9d6547ab022911b87f78e3b78ae3c69a7176d4
dsaglam94 <d.saglam94@outlook.fr>
2022-04-15 07:56:18 +0300

    added new solution

--- 7kyus.js
+++ 7kyus.js
@@ -1600,3 +1600,27 @@ function partlist(arr) {
   
   return returnArray;
 }
+
+// 15.04.2022
+
+// Given a positive integer n, calculate the following sum:
+
+// n + n/2 + n/4 + n/8 + ...
+// All elements of the sum are the results of integer division.
+
+// Example
+// 25  =>  25 + 12 + 6 + 3 + 1 = 47
+
+function halvingSum(n) {
+  if(n === 1) return n;
+  return n + halvingSum(Math.floor(n/2));
+}
+// diff solution
+function halvingSum(n) {
+  let sum = 0;
+  while(n > 0) {
+    sum += n;
+    n = Math.floor(n / 2);
+  }
+  return sum;
+}
