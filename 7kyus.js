79b81de272f2bb8fb8d8900670d5ba18a68a1f75
dsaglam94 <d.saglam94@outlook.fr>
2022-04-15 09:34:07 +0300

    added new solution

--- 7kyus.js
+++ 7kyus.js
@@ -1737,3 +1737,11 @@ function inviteMoreWomen(L) {
 function digits(n) {
   return String(n).length;
 }
+
+// Find the sum of the odd numbers within an array, after cubing the initial integers. The function should return undefined if any of the values aren't numbers.
+
+function cubeOdd(arr) {
+  arr = arr.filter((num) => {return num % 2 != 0});
+  arr = arr.reduce((a, b) => a + Math.pow(b, 3), 0);
+  return isNaN(arr) ? undefined : arr;
+}
