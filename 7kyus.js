3bf788c5adb27d52a2cfb46a42acee0c66ccaa17
dsaglam94 <d.saglam94@outlook.fr>
2022-04-17 22:36:30 +0300

    added new solution

--- 7kyus.js
+++ 7kyus.js
@@ -2027,3 +2027,27 @@ function f(n){
     
   return (n % 1 !== 0 || n <= 0)? false : (((n + 1) * n) / 2) ;
 };
+
+// #Get the averages of these numbers
+
+// Write a method, that gets an array of integer-numbers and return an array of the averages of each integer-number and his follower, if there is one.
+
+// Example:
+
+// Input:  [ 1, 3, 5, 1, -10]
+// Output:  [ 2, 4, 3, -4.5]
+// If the array has 0 or 1 values or is null, your method should return an empty array.
+
+function averages(numbers) {
+  if (numbers === null || numbers.length <= 1) {
+    return [];
+  }
+  let r = [];
+  
+  for (let i = 0; i < numbers.length; i++) {
+    r.push(numbers[i] + numbers[i+1]);
+  }
+  r.pop();
+  
+  return r.map(number => number / 2)
+}
