ae1ebb5ffb0d59716ba038cdd088e4c71dea26f9
dsaglam94 <d.saglam94@outlook.fr>
2022-04-17 07:18:22 +0300

    added another solution

--- 7kyus.js
+++ 7kyus.js
@@ -1988,3 +1988,25 @@ const evenNumbers = (array, number) => array.filter(item => item % 2 === 0).slic
 function overTheRoad(address, n){
   return (n*2+1)-address;
 }
+
+// We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
+
+// So given a string "super", we should return a list of [2, 4].
+
+// Some examples:
+// Mmmm  => []
+// Super => [2,4]
+// Apple => [1,5]
+// YoMama -> [1,2,4,6]
+
+function vowelIndices(word){
+  const str = 'aeiouy';
+  const arr = word.toLowerCase().split('');
+  let answer = [];
+  arr.forEach((name, i) => {
+    if (str.indexOf(name) !== -1) {
+      answer.push(i + 1);
+    }
+  });
+  return answer;
+}
