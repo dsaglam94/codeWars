700e4964d8dcd5860f1ebd2de707b188b5957ba1
dsaglam94 <d.saglam94@outlook.fr>
2022-04-18 10:00:48 +0300

    added new one

--- 7kyus.js
+++ 7kyus.js
@@ -2200,7 +2200,31 @@ function repeats(arr){
       }
     }
    return [...map];
-    
-    
   }
-    
+
+// The starting array sequence is [1,2,3,4,5,6,7,8,9]
+// The mixed array with one deleted number is [3,2,4,6,7,8,1,9]
+// Your function should return the int 5.
+// If no number was deleted from the starting array, your function should return the int 0.
+
+  function findDeletedNumber(arr, mixArr) {
+    return arr.filter(v => mixArr.indexOf(v) === -1)[0] || 0;
+  }
+// diff solution
+function findDeletedNumber(arr, mixArr) {
+  let arrSum = arr.reduce((a,b) => a + b, 0)
+  let mixArrSum = mixArr.reduce((a,b) => a + b, 0)
+  return arrSum - mixArrSum
+}
+// diff solution
+function findDeletedNumber(arr, mixArr) {
+  if(arr.length === mixArr.length) return 0;
+  for(var k in arr)
+    if(mixArr.indexOf(arr[k]) === -1)
+      return arr[k];
+  return 0;
+}
+// diff solution
+function findDeletedNumber(arr, mixArr) {
+  return arr.find ((element) => {return !mixArr.includes (element)}) || 0;
+}
