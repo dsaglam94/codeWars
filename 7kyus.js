37a949abf1089269705c09f3165f9670443d2768
dsaglam94 <d.saglam94@outlook.fr>
2022-04-19 07:56:41 +0300

    added new solution

--- 7kyus.js
+++ 7kyus.js
@@ -2228,3 +2228,56 @@ function findDeletedNumber(arr, mixArr) {
 function findDeletedNumber(arr, mixArr) {
   return arr.find ((element) => {return !mixArr.includes (element)}) || 0;
 }
+
+// 19.04.2022
+
+// Your task is to return an array where each object will have a new property 'greeting' with the following string value:
+
+// Hi < firstName here >, what do you like the most about < language here >?
+
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+//   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+//   { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+// ];
+// your function should return the following array:
+
+// [
+//   { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java',
+//     greeting: 'Hi Sofia, what do you like the most about Java?'
+//   },
+//   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python',
+//     greeting: 'Hi Lukas, what do you like the most about Python?'
+//   },
+//   { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby',
+//     greeting: 'Hi Madison, what do you like the most about Ruby?'
+//   } 
+// ];
+
+function greetDevelopers(list) {
+    
+  let greetingText = '';
+  
+  for (let i = 0; i < list.length; i++) {
+    greetingText = `Hi ${list[i].firstName}, what do you like the most about ${list[i].language}?`;
+    list[i].greeting = greetingText
+  }
+  
+  return list;
+}
+
+function greetDevelopers(list) {
+  list.forEach(function(developer) {
+    developer.greeting = `Hi ${developer.firstName}, what do you like the most about ${developer.language}?`;
+  });
+  
+  return list;
+}
+
+// diff 
+
+const greetDevelopers = list => list.map(
+  dev => ({...dev, greeting: `Hi ${dev.firstName}, what do you like the most about ${dev.language}?`})
+);
