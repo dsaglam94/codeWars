87d3ca3ef3877562e8678ce715f8daa34c028228
dsaglam94 <d.saglam94@outlook.fr>
2022-04-20 22:05:25 +0300

    added new one

--- 7kyus.js
+++ 7kyus.js
@@ -2448,3 +2448,21 @@ function isRubyComing(list) {
 function isRubyComing(list) {
   return list.some(e => e.language === 'Ruby');
 }
+
+// In this Kata, you will be given an array of integers whose elements have both a negative and a positive value, except for one integer that is either only negative or only positive. Your task will be to find that integer.
+
+// Examples:
+
+// [1, -1, 2, -2, 3] => 3
+
+// 3 has no matching negative appearance
+
+// [-3, 1, 2, 3, -1, -4, -2] => -4
+
+// -4 has no matching positive appearance
+
+// [1, -1, 2, -2, 3, 3] => 3
+
+// (the only-positive or only-negative integer may appear more than once)
+
+const solve = arr => [...new Set(arr)].reduce((a, c) => a + c);
