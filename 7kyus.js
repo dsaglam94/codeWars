d073c2cd1f752bf14f375a4839312fc9408ad618
Esil Önür <esilonur@192.168.1.100>
2022-04-20 11:18:22 +0300

    added a solution

--- 7kyus.js
+++ 7kyus.js
@@ -2304,6 +2304,22 @@ const myLanguages = results => Object.entries(results)
 
   // 20.04.2022
 
-  function test () {
-    console.log('hello')
-  }
+  /* Given a string s, write a method (function) that will return true if its a valid single integer or floating number or false if its not.
+
+  Valid examples, should return true:
+  
+  isDigit("3")
+  isDigit("  3  ")
+  isDigit("-3.23")
+  should return false:
+  
+  isDigit("3-4")
+  isDigit("  3   5")
+  isDigit("3 5")
+  isDigit("zero")
+*/
+
+  function isDigit(s) {
+
+    return s == parseFloat(s)
+}
