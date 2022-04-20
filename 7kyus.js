45ca2bde249a5335b740ed42826d012750e4687c
Esil Önür <esilonur@192.168.1.100>
2022-04-20 12:04:38 +0300

    added another solution

--- 7kyus.js
+++ 7kyus.js
@@ -2376,3 +2376,20 @@ function sumEvenNumbers(input) {
   
   return res;
 }
+
+// different better solution
+sumEvenNumbers = input => input.filter(x => x % 2 == 0).reduce((x, y) => x + y, 0)
+
+// Rules
+// you can put at most 8 eggs into the pot at once
+// it takes 5 minutes to boil an egg
+// we assume, that the water is boiling all the time (no time to heat up)
+// for simplicity we also don't consider the time it takes to put eggs into the pot or get them out of it
+// Example (Input --> Output)
+// 0 --> 0
+// 5 --> 5
+// 10 --> 10
+
+function cookingTime(eggs) {
+  return 5 * Math.ceil(eggs / 8);
+}
