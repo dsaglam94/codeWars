d93814891cff6a85101eebc0cee01b0de96ec4fa
dsaglam94 <d.saglam@outlook.fr>
2022-04-27 07:45:07 +0300

    new one

--- 7kyus.js
+++ 7kyus.js
@@ -3034,3 +3034,37 @@ function countChar(s, c) {
 function productArray(numbers){
   return numbers.map(x => numbers.reduce((a,b) => a * b) / x)
 }
+
+// "Chicago"  -->  "c:**,h:*,i:*,a:*,g:*,o:*"
+// As you can see, the letter c is shown only once, but with 2 asterisks.
+
+// The return string should include only the letters (not the dashes, spaces, apostrophes, etc). There should be no spaces in the output, and the different letters are separated by a comma (,) as seen in the example above.
+
+// Note that the return string must list the letters in order of their first appearance in the original string.
+
+// More examples:
+
+// "Bangkok"    -->  "b:*,a:*,n:*,g:*,k:**,o:*"
+// "Las Vegas"  -->  "l:*,a:**,s:**,v:*,e:*,g:*"
+
+function getStrings(city) {
+  city = city.toLowerCase();
+  let obj = {};
+  let str = '';
+
+  for (let elem of city) {
+      if (!(elem in obj)) {
+          obj[elem] = '*';
+      } else {
+          obj[elem] += '*';
+      }
+  }
+
+  for (let key in obj) {
+      if (key !== ' ') {
+          str += key + ':' + obj[key] + ',';
+      }
+  }
+
+  return str.substring(0, str.length - 1);
+}
