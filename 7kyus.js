440c67080abc4bd5cb3dee462b689da85f3c2f13
dsaglam94 <d.saglam@outlook.fr>
2022-04-27 07:54:17 +0300

    added new one

--- 7kyus.js
+++ 7kyus.js
@@ -3068,3 +3068,24 @@ function getStrings(city) {
 
   return str.substring(0, str.length - 1);
 }
+
+// Given a string, return true if the first instance of "x" in the string is immediately followed by the string "xx".
+
+// tripleX("abraxxxas") → true
+// tripleX("xoxotrololololololoxxx") → false
+// tripleX("softX kitty, warm kitty, xxxxx") → true
+// tripleX("softx kitty, warm kitty, xxxxx") → false
+// Note :
+
+// capital X's do not count as an occurrence of "x".
+// if there are no "x"'s then return false
+
+const tripleX = str => { 
+  let x = str.indexOf('x')
+  return x > -1 && x === str.indexOf('xxx') 
+}
+
+// diff solution
+function tripleX(str){
+  return str.substr(str.indexOf('x')+1,2) === 'xx';
+}
