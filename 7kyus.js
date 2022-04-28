d46f294735684384dc1a70043a657fd1462de336
dsaglam94 <d.saglam@outlook.fr>
2022-04-28 12:15:53 +0300

    new one

--- 7kyus.js
+++ 7kyus.js
@@ -3350,3 +3350,17 @@ const arrayLeaders = numbers => {
     
     return reversed;
   }
+
+//   Input >> Output Examples
+// maxProduct ({4, 3, 5}, 2) ==>  return (20)
+// Explanation:
+// Since the size (k) equal 2 , then the subsequence of size 2 whose gives product of maxima is 5 * 4 = 20 .
+// maxProduct ({8, 10 , 9, 7}, 3) ==>  return (720)
+// Explanation:
+// Since the size (k) equal 3 , then the subsequence of size 3 whose gives product of maxima is 8 * 9 * 10 = 720 .
+
+  function maxProduct(numbers, size){
+    numbers = numbers.sort((a,b) => a - b).slice(-size);
+    return numbers.reduce((x,y) => x * y, 1)
+    
+  }
