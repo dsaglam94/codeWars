281dcc9e1689aa88eec6fe0dfbbd650e1f7efbdf
dsaglam94 <d.saglam@outlook.fr>
2022-04-28 11:44:17 +0300

    new solution

--- 7kyus.js
+++ 7kyus.js
@@ -3301,3 +3301,7 @@ function arrayLeaders(numbers){
   
   return output;
 }
+
+// diff and smarter solution
+const arrayLeaders = numbers => {
+  return numbers.filter((a, i) => numbers.slice(i + 1).reduce((sum, b) => sum + b, 0) < a)}
