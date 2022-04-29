158b24600e8ced884177c80bb34f64295db95f5f
dsaglam94 <d.saglam@outlook.fr>
2022-04-29 19:41:12 +0300

    another one

--- 7kyus.js
+++ 7kyus.js
@@ -3497,3 +3497,20 @@ const findAll = (nums, find) => nums
     while(new Set([...++a+'']).size<4);
     return a;
   }
+
+// Define a method/function that removes from a given array of integers all the values contained in a second array.
+
+// Examples (input -> output):
+// * [1, 1, 2 ,3 ,1 ,2 ,3 ,4], [1, 3] -> [2, 2, 4]
+// * [1, 1, 2 ,3 ,1 ,2 ,3 ,4, 4, 3 ,5, 6, 7, 2, 8], [1, 3, 4, 2] -> [5, 6 ,7 ,8]
+// * [8, 2, 7, 2, 3, 4, 6, 5, 4, 4, 1, 2 , 3], [2, 4, 3] -> [8, 7, 6, 5, 1]
+
+
+  Array.prototype.remove_ = function(integer_list, values_list){
+    return integer_list.filter(function (element) {
+      return values_list.indexOf(element) === -1;
+    });
+  }
+
+  // diff solution
+  Array.prototype.remove_ = (x, y) => x.filter(e => !y.includes(e));
