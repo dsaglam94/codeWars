4a81bf4b1a6beab31bd1e2ecebbdf6321cb2aee0
dsaglam94 <d.saglam@outlook.fr>
2022-04-29 17:54:05 +0300

    added new one

--- 7kyus.js
+++ 7kyus.js
@@ -3381,11 +3381,33 @@ const arrayLeaders = numbers => {
   }
 
   // 29.04.2022 
-  
+
   // Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
 
   // If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
 
   function last(x){
     return x.split(' ').sort((a, b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1));
+  }
+
+//   Given an array (a list in Python) of integers and an integer n, find all occurrences of n in the given array and return another array containing all the index positions of n in the given array.
+
+// If n is not in the given array, return an empty array [].
+
+// Assume that n and all values in the given array will always be integers.
+
+// Example:
+
+findAll([6, 9, 3, 4, 3, 82, 11], 3) => [2, 4]
+
+  const findAll = (array, n) => {
+    let output = [];
+    
+    for (let i = 0; i < array.length; i++) {
+        if ( array[i] === n) {
+          output.push(i);
+        }
+    }
+    
+    return output;
   }
