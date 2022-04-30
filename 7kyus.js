4661588fd05a9f3f0126f0e71e2524313bee2f4e
dsaglam94 <d.saglam@outlook.fr>
2022-04-30 12:17:41 +0300

    new one

--- 7kyus.js
+++ 7kyus.js
@@ -3641,3 +3641,44 @@ function init(array) {
 function last(array) {
   return array[array.length-1];
 }
+
+// Write a method, that replaces every nth char oldValue with char newValue.
+
+// Method:
+
+// replaceNth(text, n, oldValue, newValue)
+// Example:
+// n:         2
+// oldValue: 'a'
+// newValue: 'o'
+// "Vader said: No, I am your father!" -> "Vader soid: No, I am your fother!"
+//   1     2          3        4       -> 2nd and 4th occurence are replaced
+// Your method has to be case sensitive!
+
+// As you can see in the example: The first changed is the 2nd 'a'. So the start is always at the nth suitable char and not at the first!
+
+// If n is 0 or negative or if it is larger than the count of the oldValue, return the original text without a change.
+
+// The text and the chars will never be null.
+
+function replaceNth(text, n, oldValue, newValue) {  
+  var count = 0;
+  if(n <= 0){
+    return text;
+  }
+  console.log(n);
+  return text.split("").map(function(item, index){
+    if(item === oldValue){
+      count++;
+    }
+    if(item === oldValue && count % n === 0){
+      console.log(item, count, index);
+      return newValue;
+    }else{
+      return item;
+    }
+  }).join("");
+}
+
+// Diff solution
+const replaceNth = (text, n, oldValue, newValue) => n <= 0 || n > text.split(oldValue).length ? text : text.split(oldValue).map((v,i) => (i+1) % n ? v += oldValue : v += newValue).join('').slice(0, -1);
