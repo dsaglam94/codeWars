392fc77d0496121efa415e66dad7d3e4e2818a14
dsaglam94 <d.saglam@outlook.fr>
2022-04-30 21:04:08 +0300

    another one added

--- 7kyus.js
+++ 7kyus.js
@@ -3714,3 +3714,25 @@ function findAdmin(list, lang) {
   
       return res;
 }
+
+// Diff solution with filter
+function findAdmin(list, lang) {
+  return list.filter(function(dev){return dev.language == lang && dev.githubAdmin == 'yes'});
+}
+
+// ou have to create a method, that corrects a given time string.
+// There was a problem in addition, so many of the time strings are broken.
+// Time is formatted using the 24-hour clock, so from 00:00:00 to 23:59:59.
+// Examples
+// "09:10:01" -> "09:10:01"  
+// "11:70:10" -> "12:10:10"  
+// "19:99:99" -> "20:40:39"  
+// "24:01:01" -> "00:01:01"  
+
+function timeCorrect(str) {
+  const date = new Date(); 
+  if (str == '') return str;
+  if (!/^\d{2}\:\d{2}\:\d{2}$/g.test(str)) return null;
+  date.setUTCHours(...str.split(':'));
+  return date.toLocaleTimeString('en', {hour12: false});
+}
