d2882d0bd1055d52cba7c22c910a8e1295ef72de
dsaglam94 <d.saglam@outlook.fr>
2022-05-01 08:21:16 +0300

    another solution

--- 7kyus.js
+++ 7kyus.js
@@ -3844,3 +3844,15 @@ function scrollingText(text){
 function howManyTimes(annualPrice, individualPrice) {
   return Math.ceil(annualPrice / individualPrice);
 }
+
+// Given a positive integer N, return the largest integer k such that 3^k < N.
+
+// For example,
+
+// largest_power(3) == 0
+// largest_power(4) == 1
+// You may assume that the input to your function is always a positive integer.
+
+function largestPower(n){
+  return Math.ceil(Math.log10(n)/Math.log10(3))-1
+}
