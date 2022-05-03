d401f108be698a1482bb0a6cc6836d077470db5f
dsaglam94 <d.saglam@outlook.fr>
2022-05-03 07:44:46 +0300

    added new solution

--- 7kyus.js
+++ 7kyus.js
@@ -4119,3 +4119,30 @@ function splitTheBill(x) {
   }
   
   const sum = (a, b) => a + b;
+
+  // 03.05.2022
+
+//   Impliment the reverse function, which takes in input n and reverses it. For instance, reverse(123) should return 321. You should do this without converting the inputted number into a string.
+
+// // Please do not use
+// const forbidden = "
+//                   '
+//                   `
+//                   string
+//                   fixed
+//                   precision
+//                   .keys
+
+  function reverse(n){
+    let revNumber = 0;
+     while (n > 0) {
+       revNumber = (revNumber * 10) + (n % 10);
+       n = Math.floor(n / 10);
+     }
+     return revNumber;
+   }
+
+  //  diff solution
+  reverse=n=>+([]+n).split([]+[]).reverse().join([]+[])
+  // diff solution
+  let reverse = n => +Array.of(n).join().split([]).reverse().join([])
