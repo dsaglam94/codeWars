fb21fe13f632ff5f39042fbef2e0a088159eddf4
dsaglam94 <d.saglam@outlook.fr>
2022-05-03 09:18:19 +0300

    another solution added

--- 7kyus.js
+++ 7kyus.js
@@ -4260,3 +4260,23 @@ function splitTheBill(x) {
   function pyramid(balls) {
     return Math.floor((Math.sqrt(balls * 8 + 1) - 1) / 2);
   }
+
+// A 6-sided die is rolled a number of times and the results are plotted as a character-based histogram.
+
+// 6|##### 5
+// 5|
+// 4|# 1
+// 3|########## 10
+// 2|### 3
+// 1|####### 7
+// Task
+// You will be passed the dice value frequencies, and your task is to write the code to return a string representing a histogram, so that when it is printed it has the same format as the example.
+
+  function histogram(results) {
+    var histogram = '';
+    
+    for (var i=5; i>=0; --i)
+      histogram += (i+1) + '|' + '#'.repeat(results[i]) + (results[i] > 0 ? ' ' + results[i] : '') + "\n";
+    
+    return histogram;
+  }
