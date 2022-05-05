e5f95b71d06096b570b6efb9b73862f046776bae
dsaglam94 <d.saglam@outlook.fr>
2022-05-05 09:37:34 +0300

    added a solution

--- 7kyus.js
+++ 7kyus.js
@@ -4581,3 +4581,30 @@ function spread(func, args) {
 function spread(func, args) {
   return func.apply(func, args)
 }
+
+// I. Cuboid
+// The object constructor for the class Cuboid should receive exactly three arguments in the following order: length, width, height and store these three values in this.length, this.width and this.height respectively.
+
+// The class Cuboid should then have a getter surfaceArea which returns the surface area of the cuboid and a getter volume which returns the volume of the cuboid.
+
+// II. Cube
+// class Cube is a subclass of class Cuboid. The constructor function of Cube should receive one argument only, its length, and use that value passed in to set this.length, this.width and this.height.
+
+class Cuboid {
+  constructor(length, width, height) {
+    this.length = length;
+    this.width = width;
+    this.height = height;
+  }
+  get volume() {
+    return this.length * this.width * this.height;
+  }
+  get surfaceArea() {
+    return 2 * (this.length * this.width + this.width * this.height + this.height * this.length);
+  }
+}
+class Cube extends Cuboid {
+  constructor(length) {
+    super(length, length, length);
+  }
+}
