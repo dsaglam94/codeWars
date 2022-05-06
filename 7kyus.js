64b02dc928bfa2aa038f9fa24782a4313c297bb5
dsaglam94 <d.saglam@outlook.fr>
2022-05-06 07:55:48 +0300

    added diff solution

--- 7kyus.js
+++ 7kyus.js
@@ -4637,3 +4637,8 @@ function bingo(a) {
            .map(x => x.charCodeAt(0)-96)
            .every(o => a.includes(o))) ? "WIN" : "LOSE" ;
   }
+
+  // diff solution but not a good practice.
+  // Upper one is more robust
+
+  const bingo = ar => [2,7,9,14,15].every(e => ar.includes(e)) ? 'WIN' : 'LOSE';
