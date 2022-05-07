3debe45b8f883fdac5737e252c09966329d43afb
dsaglam94 <d.saglam@outlook.fr>
2022-05-07 08:14:19 +0300

    added another one

--- 8kyus.js
+++ 8kyus.js
@@ -227,3 +227,11 @@ function sameCase(a, b){
   }
   return (a.toLowerCase() === a) === (b.toLowerCase() === b) ? 1 : 0;
 }
+
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// nameShuffler('john McClane'); => "McClane john"
+
+function nameShuffler(str){
+  return str.split(' ').reverse().join(' ');
+}
