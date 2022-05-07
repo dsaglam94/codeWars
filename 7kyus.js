c6cce04ff6f98078e679cfafa6a23dc82262fc6d
dsaglam94 <d.saglam@outlook.fr>
2022-05-07 07:27:21 +0300

    added a solution

--- 7kyus.js
+++ 7kyus.js
@@ -4695,3 +4695,25 @@ function* baumSweet() {
     n++;    
   }
 }
+
+// 07.05.2022
+
+// Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+
+function chunkArrayInGroups(arr, size) {
+  let temp = [];
+  let result = [];
+
+  for (let a = 0; a < arr.length; a++) {
+    if (a % size !== size - 1) temp.push(arr[a]);
+    else {
+      temp.push(arr[a]);
+      result.push(temp);
+      temp = [];
+    }
+  }
+
+  if (temp.length !== 0) result.push(temp);
+  return result;
+}
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
