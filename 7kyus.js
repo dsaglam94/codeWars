ecda56a4156c2baaff4179cd217d890f026441f7
dsaglam94 <d.saglam@outlook.fr>
2022-05-08 22:55:28 +0300

    another solution added

--- 7kyus.js
+++ 7kyus.js
@@ -4896,3 +4896,20 @@ function appendArrays (arr1, arr2) {
   return [...arr1, ...arr2]
   
 }
+
+// Write a function that can return the smallest value of an array or the index of that value. The function's 2nd parameter will tell whether it should return the value or the index.
+
+// Assume the first parameter will always be an array filled with at least 1 number and no duplicates. Assume the second parameter will be a string holding one of two values: 'value' and 'index'.
+
+// min([1,2,3,4,5], 'value') // => 1
+// min([1,2,3,4,5], 'index') // => 0
+
+function min(arr, toReturn) {
+  let newArr = arr.slice().sort((a,b) => a-b);
+
+  if(toReturn === 'value') {
+      return newArr[0];
+  } else if (toReturn === 'index') {
+      return arr.indexOf(newArr[0])
+  }
+}
