725f85e122ce6f61ef100e13b3917f03a2f29b59
dsaglam94 <d.saglam@outlook.fr>
2022-05-08 22:18:38 +0300

    another solution added

--- 7kyus.js
+++ 7kyus.js
@@ -4835,3 +4835,17 @@ function isAllPossibilities(x){
 function isAllPossibilities(x){
   return x.length > 0 ? x.every((a,i) => x.includes(i)) : false;
 }
+
+// Some really funny web dev gave you a sequence of numbers from his API response as an sequence of strings!
+
+// You need to cast the whole array to the correct type.
+
+// Create the function that takes as a parameter a sequence of numbers represented as strings and outputs a sequence of numbers.
+
+// ie:["1", "2", "3"] to [1, 2, 3]
+
+// Floats are possible
+
+function toNumberArray(arr){
+  return arr.map(parseFloat)
+}
