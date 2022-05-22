65821c67e80d48af3921c7de2a05bf226b0f783d
dsaglam94 <d.saglam@outlook.fr>
2022-05-22 10:38:35 +0300

    added another solution

---
+++ 6th_kyus/replaceWithAlpPosition.js
@@ -0,0 +1,37 @@
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+function alphabetPosition(text) {
+    return (text.match(/[A-Za-z]/g) || []).map(getCharCodeFrom1).join(' ');
+  }
+  
+  function getCharCodeFrom1(str) {
+    return str.toUpperCase().charCodeAt(0) - 64
+}
+
+// this one doesn't handle the null 
+function alphabetPosition(text) {
+    return text
+      .toUpperCase()
+      .match(/[a-z]/gi)
+      .map( (c) => c.charCodeAt() - 64)
+      .join(' ');
+}
+
+// with old friendly for loop
+function alphabetPosition(text) {
+    var result = "";
+    for (var i = 0; i < text.length; i++){
+      var code = text.toUpperCase().charCodeAt(i)
+      if (code > 64 && code < 91) result += (code - 64) + " ";
+    }
+  
+    return result.trim()
+}
