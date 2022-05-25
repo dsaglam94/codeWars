c97eb2b25cdd5bf7efde32a135f00a7cc72d8cf9
dsaglam94 <d.saglam@outlook.fr>
2022-05-25 09:10:12 +0300

    refactored the code and added a different solution

--- 6th_kyus/buildTower.js
+++ 6th_kyus/buildTower.js
@@ -9,11 +9,19 @@
 // ]
 
 function towerBuilder(nFloors) {
-    var tower = [];
-    for (var i = 0; i < nFloors; i++) {
+    let tower = [];
+    for (let i = 0; i < nFloors; i++) {
       tower.push(" ".repeat(nFloors - i - 1)
                + "*".repeat((i * 2)+ 1)
                + " ".repeat(nFloors - i - 1));
     }
     return tower;
+}
+
+// diff solution
+function towerBuilder(n) {
+    return Array.from({length: n}, function(v, k) {
+      const spaces = ' '.repeat(n - k - 1);
+      return spaces + '*'.repeat(k + k + 1) + spaces;
+    });
 }
