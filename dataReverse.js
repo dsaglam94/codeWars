52911155fc4d4a8ff22a56800b6e548f63be833e
dsaglam94 <d.saglam@outlook.fr>
2022-06-06 08:19:39 +0300

    another one

--- dataReverse.js
+++ dataReverse.js
@@ -20,3 +20,12 @@ function dataReverse(data) {
     return myArr.reverse().join('').split('').map(Number)
   
 }
+
+// diff approach
+const dataReverse = data => {
+    const bytes = [];
+    for (let i = 0; i < data.length; i += 8) {
+      bytes.unshift(...data.slice(i, i + 8));
+    }
+    return bytes;
+}
