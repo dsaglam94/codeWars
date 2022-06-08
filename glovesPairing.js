23ba186ebfbf261dc10bdd5ec4e4af4603735319
dsaglam94 <d.saglam@outlook.fr>
2022-06-08 08:42:03 +0300

    added diff solution

--- glovesPairing.js
+++ glovesPairing.js
@@ -21,3 +21,21 @@ function numberOfPairs(gloves){
     }
     return pairs
 }
+
+// different solution
+function numberOfPairs(gloves) {
+    let pairs = 0;
+    let count = {}
+    for (let color of gloves) {
+      if (color in count) {
+        count[color]++;
+      } else {
+        count[color] = 1;
+      }
+      if (count[color] === 2) {
+        pairs++;
+        count[color] = 0;
+      }
+    }
+    return pairs;
+}
