60e56fc09a43f1246060da4a8af642345f43c03c
dsaglam94 <d.saglam@outlook.fr>
2022-06-09 07:53:19 +0300

    another solution added

---
+++ removeWordsIf1ExclamationMarkExists.js
@@ -0,0 +1,29 @@
+// Remove words from the sentence if they contain exactly one exclamation mark. Words are separated by a single space, without leading/trailing spaces.
+
+// Examples
+// remove("Hi!") === ""
+// remove("Hi! Hi!") === ""
+// remove("Hi! Hi! Hi!") === ""
+// remove("Hi Hi! Hi!") === "Hi"
+// remove("Hi! !Hi Hi!") === ""
+// remove("Hi! Hi!! Hi!") === "Hi!!"
+// remove("Hi! !Hi! Hi!") === "!Hi!"
+
+function remove (string) {
+    let splittedStr = string.split(' ')
+    let r = [];
+    
+    for(let i = 0; i < splittedStr.length; i++) {
+        let count = 0
+        for(let j = 0; j < splittedStr[i].length; j++) {
+          if(splittedStr[i][j] === '!') {
+            count++
+          }
+        }
+        if(count !== 1) {
+          r.push(splittedStr[i])
+        }
+    }
+    
+    return r.join(' ')
+}
