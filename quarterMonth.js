79ad8990e39527f1fe12b1331fce255b3391ccab
dsaglam94 <d.saglam@outlook.fr>
2022-06-20 23:59:25 +0300

    refactored the code

--- quarterMonth.js
+++ quarterMonth.js
@@ -18,3 +18,9 @@ const quarterOf = (month) => {
 const quarterOf2 = (month) => {
   return Math.ceil(month / 3);
 };
+
+// ternary
+
+const quarterOf3 = (month) => {
+  return month < 4 ? 1 : month < 7 ? 2 : month < 10 ? 3 : 4;
+};
