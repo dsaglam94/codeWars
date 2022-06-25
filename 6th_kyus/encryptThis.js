556a38a9527cb927078b0d9c8afb128511f0bb81
dsaglam94 <d.saglam@outlook.fr>
2022-06-25 13:34:10 +0300

    added 6th kyu solution

---
+++ 6th_kyus/encryptThis.js
@@ -0,0 +1,45 @@
+// Encrypt this!
+
+// You want to create secret messages which can be deciphered by the Decipher this! kata. Here are the conditions:
+
+// Your message is a string containing space separated words.
+// You need to encrypt each word in the message using the following rules:
+// The first letter must be converted to its ASCII code.
+// The second letter must be switched with the last letter
+// Keepin' it simple: There are no special characters in the input.
+// Examples:
+// encryptThis("Hello") === "72olle"
+// encryptThis("good") === "103doo"
+// encryptThis("hello world") === "104olle 119drlo"
+
+const encryptThis = (text) => {
+  let encrypted = "";
+  const textArr = text.slice().split(" ");
+
+  for (let i = 0; i < textArr.length; i++) {
+    let temp = "";
+    if (temp === "") {
+      if (textArr[i].length === 2) {
+        temp += textArr[i].charCodeAt(0) + textArr[i][1];
+        encrypted += temp + " ";
+        temp = "";
+      } else if (textArr[i].length === 1) {
+        temp += textArr[i].charCodeAt(0);
+        encrypted += temp + " ";
+        temp = "";
+      } else {
+        let len = textArr[i].length - 1;
+        let secondLetter = textArr[i][1];
+        let lastLetter = textArr[i][len];
+        let remainingLetters = textArr[i].slice(2, len);
+
+        let restOfWord = lastLetter + remainingLetters + secondLetter;
+
+        temp += textArr[i].charCodeAt(0) + restOfWord;
+        encrypted += temp + " ";
+        temp = "";
+      }
+    }
+  }
+  return encrypted.trim();
+};
